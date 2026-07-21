efbd4330dda2b24947e866d9c1d6a06cd565399d
xdoouglasxxxx <xdoouglasxxxx@gmail.com>
2026-07-21 05:51:59 -0300

    Update button.tsx

--- mh-studio-painel/components/ui/button.tsx
+++ mh-studio-painel/components/ui/button.tsx
@@ -13,7 +13,7 @@ const buttonVariants = cva(
         outline: "border border-input bg-transparent shadow-sm hover:bg-accent",
         ghost: "hover:bg-accent hover:text-accent-foreground"
       },
-      size: { default: "h-9 px-4 py-2", lg: "h-12 px-8 text-base" }
+      size: { default: "h-9 px-4 py-2", sm: "h-8 px-3 text-xs", lg: "h-12 px-8 text-base", icon: "h-9 w-9" }
     },
     defaultVariants: { variant: "default", size: "default" }
   }
